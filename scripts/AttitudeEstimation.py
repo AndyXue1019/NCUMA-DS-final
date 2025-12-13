f6fec03d4ee903549a6b4fcd07de912b7e6e21db
An-You Xue <a931019ing@gmail.com>
2025-12-14 01:22:21 +0800

    correct the coordinate transformation and add marker

--- scripts/AttitudeEstimation.py
+++ scripts/AttitudeEstimation.py
@@ -6,6 +6,7 @@ import rospy
 from geometry_msgs.msg import Vector3
 from sensor_msgs.msg import Imu, MagneticField
 from tf.transformations import euler_from_quaternion
+from visualization_msgs.msg import Marker
 
 from ekf import EKF
 
@@ -18,6 +19,7 @@ class AttitudeEstimationNode:
         self.last_time = None
 
         self.pose_pub = rospy.Publisher('/imu/pose/filtered', Vector3, queue_size=10)
+        self.marker_pub = rospy.Publisher('/imu/marker', Marker, queue_size=10)
 
         imu_sub = message_filters.Subscriber('/imu/data_raw', Imu)
         mag_sub = message_filters.Subscriber('/imu/mag', MagneticField)
@@ -89,9 +91,9 @@ class AttitudeEstimationNode:
         gyro_corrected = raw_gyro - self.gyro_bias
 
         # Coordinate Transformation
-        gyro = np.array([-gyro_corrected[2], -gyro_corrected[0], -gyro_corrected[1]])
-        acc = np.array([-raw_acc[2], -raw_acc[0], -raw_acc[1]])
-        mag = np.array([-raw_mag[2], -raw_mag[0], -raw_mag[1]])
+        gyro = np.array([-gyro_corrected[2], -gyro_corrected[1], -gyro_corrected[0]])
+        acc = np.array([-raw_acc[2], -raw_acc[1], -raw_acc[0]])
+        mag = np.array([-raw_mag[2], -raw_mag[1], -raw_mag[0]])
 
         self.ekf.predict(gyro, dt)
         self.ekf.update(acc, np.array([0, 0, 1]), self.ekf.R_acc)
@@ -114,6 +116,46 @@ class AttitudeEstimationNode:
         msg.z = yaw
 
         self.pose_pub.publish(msg)
+
+        self.publish_marker(q)
+
+    def publish_marker(self, q):
+        marker = Marker()
+        # [重要] Frame ID 設為 "world"，等一下 Rviz 的 Fixed Frame 也要設成這個
+        marker.header.frame_id = 'world'
+        marker.header.stamp = rospy.Time.now()
+
+        # 設定 Marker 類型為 CUBE (立方體)
+        marker.ns = 'imu_shape'
+        marker.id = 0
+        marker.type = Marker.CUBE
+        marker.action = Marker.ADD
+
+        # 設定位置 (固定在原點)
+        marker.pose.position.x = 0
+        marker.pose.position.y = 0
+        marker.pose.position.z = 0
+
+        # 設定姿態 (從 EKF 取得的四元數)
+        # 注意: EKF 的 q 是 [w, x, y, z]，ROS msg 是 x, y, z, w
+        marker.pose.orientation.w = q[0]
+        marker.pose.orientation.x = q[1]
+        marker.pose.orientation.y = q[2]
+        marker.pose.orientation.z = q[3]
+
+        # 設定尺寸 (單位: 公尺)
+        # 做成扁平狀，模擬 IMU 模組的外觀
+        marker.scale.x = 0.1  # 長 5cm (X軸-紅色)
+        marker.scale.y = 0.5  # 寬 3cm (Y軸-綠色)
+        marker.scale.z = 0.3  # 高 1cm (Z軸-藍色)
+
+        # 設定顏色 (RGBA)
+        marker.color.r = 1.0
+        marker.color.g = 0.5
+        marker.color.b = 0.0
+        marker.color.a = 1.0  # 不透明
+
+        self.marker_pub.publish(marker)
 
 
 if __name__ == '__main__':
